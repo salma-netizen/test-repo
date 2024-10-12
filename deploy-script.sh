9f55e30e86debb065fdb8583586add9de4df66e0
Salma Elsayed <engsalmaelsayed7@gmail.com>
2024-10-12 08:17:54 +0300

    first commit

--- deploy-script.sh
+++
@@ -1,7 +0,0 @@
-gcloud auth activate-service-account --key-file google.key.json
-gcloud config set container/use_client_certificate False
-gcloud container clusters get-credentials $CI_GOOGLE_CLUSTER_NAME --zone $CI_GOOGLE_CLUSTER_ZONE --project $CI_GOOGLE_PROJECT_NAME
-
-kubectl apply -f ./repository-docker-secret.json
-kubectl apply -f ./deployments.json
-kubectl apply -f ./service-deploy.json
